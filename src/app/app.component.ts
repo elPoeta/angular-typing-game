7fedfeeefebd528a33ad1ad9deff184da776ed80
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-09 19:14:55 -0300

    create class app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from "faker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  sentence: string;
+  solved: boolean;
+  enteredText: string = "";
 
+  ngOnInit(): void {
+    this.solved = false;
+    this.sentence = lorem.sentence();
+  }
+
+  handlerInput(value: string) {
+    this.solved = this.sentence === value;
+    this.enteredText = value;
+  }
+
+  compare(letter: string, character: string): string {
+    if (!character) {
+      return "pending";
+    }
+    return letter === character ? "correct" : "incorrect";
+  }
 }
