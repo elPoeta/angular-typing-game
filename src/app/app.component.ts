7a8991614fb205eb60678ff4cf4df8950729470e
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-09-10 10:27:57 -0300

    add clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,45 @@ export class AppComponent implements OnInit {
   sentence: string;
   solved: boolean;
   enteredText: string = "";
+  isStoped: boolean;
+  countDown: number;
+  isDisabled: boolean;
 
   ngOnInit(): void {
+    this.countDown = 15;
+    this.isStoped = true;
+    this.isDisabled = true;
     this.solved = false;
     this.sentence = lorem.sentence();
   }
 
   handlerStart() {
-    console.log('start')
+    this.isStoped = false;
+    this.isDisabled = false;
+    const input: HTMLInputElement = document.querySelector('#random')! as HTMLInputElement;
+    setTimeout(() => {
+      input.focus();
+    }, 0);
+    const inter = setInterval(() => {
+      this.countDown -= 1;
+      this.clearTime(inter);
+    }, 1000);
   }
 
   handlerInput(value: string) {
     this.solved = this.sentence === value;
     this.enteredText = value;
+  }
+
+  clearTime(inter) {
+    if (this.countDown === 0) {
+      clearInterval(inter);
+      this.isDisabled = true;
+    }
+  }
+
+  fail() {
+    return !this.solved && this.isDisabled && !this.isStoped;
   }
 
   compare(letter: string, character: string): string {
